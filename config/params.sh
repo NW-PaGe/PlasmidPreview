5fd92f4ae79211dde24d7aa0478663a1e6db27d8
DOH-SEH2303 <shawn.hawken@doh.wa.gov>
2026-03-05 15:45:30 -0800

    add config

---
+++ config/params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# pipeline params- edit for your env
+
+THREADS=8
+RESULTS_DIR=~/plasmid-triage/results
+AMR_DB=~/plasmid-triage/amrfinderplus_db
+
+# Input paath- can be local dir or S3 path
+INPUT_PATH=~/plasmid-triage/assemblies
