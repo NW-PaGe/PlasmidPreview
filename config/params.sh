f2387942129a2ca5f031976a8e44413f619b279d
Ubuntu <ubuntu@ip-172-31-17-92.us-west-2.compute.internal>
2026-03-11 17:33:45 +0000

    update params

--- config/params.sh
+++ config/params.sh
@@ -4,7 +4,7 @@
 
 THREADS=8
 RESULTS_DIR=~/plasmid-triage/results
-AMR_DB=~/plasmid-triage/amrfinderplus_db
+AMR_DB=/home/ubuntu/miniconda3/envs/plasmid/share/amrfinderplus/data/2023-11-15.1
 
 # Input paath- can be local dir or S3 path
 INPUT_PATH=~/plasmid-triage/assemblies
