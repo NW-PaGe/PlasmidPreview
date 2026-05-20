cd079f742cb804b3b47ac331067e5993efa650f8
Shawn Hawken <shawn.hawken@doh.wa.gov>
2026-05-20 14:08:10 -0700

    Update params.sh for organism flag
    adding organism flag param to include point mutations and remove resistance present in all members of a species

--- config/params.sh
+++ config/params.sh
@@ -8,3 +8,9 @@ AMR_DB=/home/ubuntu/miniconda3/envs/plasmid/share/amrfinderplus/data/2023-11-15.
 
 # Input paath- can be local dir or S3 path
 INPUT_PATH=~/plasmid-triage/assemblies
+
+# AMRFinderPlus organism for species-specific point mutations
+# leave empty to skip --organism flag
+# requires underscores, not spaces ie: Klebsiella_pneumoniae
+# run amrfinder --list_organisms to see supported orgs in db version
+ORGANISM=""
