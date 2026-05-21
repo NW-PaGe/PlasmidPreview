2b040592e0ccb553fd7402b31f07c369b07031f8
Shawn Hawken <shawn.hawken@doh.wa.gov>
2026-05-21 10:59:12 -0700

    Update params.sh with input path vs hard coded

--- config/params.sh
+++ config/params.sh
@@ -9,6 +9,9 @@ AMR_DB=/home/ubuntu/miniconda3/envs/plasmid/share/amrfinderplus/data/2023-11-15.
 # Input paath- can be local dir or S3 path
 INPUT_PATH=~/plasmid-triage/assemblies
 
+# Local destination for s3 syncs (onlyu used when INPUT_PATH is an s3:// URI)
+LOCAL_ASSEMBLIES_DIR=~/plasmid-triage/assemblies
+
 # AMRFinderPlus organism for species-specific point mutations
 # leave empty to skip --organism flag
 # requires underscores, not spaces ie: Klebsiella_pneumoniae
